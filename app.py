0e7e35c4c0cbb00f9bb78503386b6f451f9cf1ed
suda-morris <362953310@qq.com>
2017-11-03 14:31:21 +0800

    增加模板文件

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # encoding: utf-8
-from flask import Flask
+from flask import Flask, render_template
 import config
 
 app = Flask(__name__)
@@ -8,12 +8,18 @@ app.config.from_object(config)
 
 @app.route("/")
 def index():
-    return "欢迎光临寒舍"
+    books = [
+        {"name": u"三国演义", "author": u"罗贯中", "price": 100},
+        {"name": u"水浒传", "author": u"施耐庵", "price": 110},
+        {"name": u"西游记", "author": u"吴承恩", "price": 120},
+        {"name": u"红楼梦", "author": u"曹雪芹", "price": 130}
+    ]
+    return render_template("index.html", books=books)
 
 
 @app.route("/article/<article_id>/")
 def article(article_id):
-    return u"您的请求参数为:{param}".format(param=article_id)
+    return u"您的请求文章ID为:{param}".format(param=article_id)
 
 
 if __name__ == "__main__":
